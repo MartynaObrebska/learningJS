33f63e50ecefe6d76cf66af9465c0ba8bf2e1d1f
Martyna Obrebska <martyna.obrebska94@gmail.com>
2022-12-04 21:47:04 +0100

    Refactor to-do app.

--- toDoApp/main.js
+++ toDoApp/main.js
@@ -1,35 +1,41 @@
-const searchInput = document.querySelector("input.search");
-const addForm = document.querySelector("form.add");
-const addInput = document.querySelector("input.add");
-const ul = document.querySelector("ul");
-const taskList = document.getElementsByClassName("task");
-const tasksNumber = document.querySelector("span.number");
+(function () {
+  const searchInput = document.querySelector("#search-input");
+  const addForm = document.querySelector("#add-task-form");
+  const addInput = document.querySelector("#add-task-input");
+  const ul = document.querySelector("ul");
+  const taskList = document.getElementsByClassName("task");
+  const tasksNumber = document.querySelector("span");
 
-const removeTask = (e) => {
-  e.target.parentNode.remove();
-  tasksNumber.textContent = taskList.length;
-};
+  const removeTask = (e) => {
+    e.target.parentNode.remove();
+    tasksNumber.textContent = taskList.length;
+  };
 
-const addTask = (e) => {
-  e.preventDefault();
-  const taskName = addInput.value;
-  const task = document.createElement("li");
-  task.classList.add("task");
-  task.innerHTML = taskName + ` <button>Remove</button>`;
-  ul.appendChild(task);
-  addInput.value = "";
-  task.querySelector("button").addEventListener("click", removeTask);
-  tasksNumber.textContent = taskList.length;
-};
+  const addTask = (e) => {
+    e.preventDefault();
+    const taskName = addInput.value.trim();
+    if (taskName === "") {
+      return;
+    }
+    const task = document.createElement("li");
+    task.classList.add("task");
+    task.innerHTML = taskName + ` <button>Remove</button>`;
+    ul.appendChild(task);
+    addInput.value = "";
+    task.childNodes[1].addEventListener("click", removeTask);
+    tasksNumber.textContent = taskList.length;
+  };
 
-const search = (e) => {
-  const tasks = [...document.querySelectorAll("li")];
-  tasks.forEach((item) => {
-    item.style.display = item.textContent.includes(e.target.value)
-      ? "list-item"
-      : "none";
-  });
-};
+  const search = (e) => {
+    [...taskList].forEach((item) => {
+      item.style.display = item.textContent
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase())
+        ? "list-item"
+        : "none";
+    });
+  };
 
-addForm.addEventListener("submit", addTask);
-searchInput.addEventListener("input", search);
+  addForm.addEventListener("submit", addTask);
+  searchInput.addEventListener("input", search);
+})();
