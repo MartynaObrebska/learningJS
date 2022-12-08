2ea8207a795c95e0c806a3711e477bdeff718d5b
Martyna Obrebska <martyna.obrebska94@gmail.com>
2022-12-08 23:42:10 +0100

    Add splits into timer app.

--- Timer App/main.js
+++ Timer App/main.js
@@ -1,33 +1,59 @@
 const startBtn = document.querySelector("#start");
 const stopBtn = document.querySelector("#stop");
 const time = document.querySelector("#time");
+const ul = document.querySelector("#splits");
+const splits = document.getElementsByClassName("split");
 let seconds = 0;
-let flag = true;
+let startActive = true;
+let stopActive = true;
 let id;
+
+const createSplit = () => {
+  const split = document.createElement("li");
+  split.classList.add("split");
+  split.textContent = time.textContent;
+  ul.appendChild(split);
+};
+
 const counter = () => {
   seconds++;
   time.textContent = (seconds / 100).toFixed(2);
 };
 
 const start = () => {
-  if (flag) {
+  if (startActive) {
     id = setInterval(counter, 10);
-    flag = !flag;
-    startBtn.textContent = "PAUSE";
+    startActive = !startActive;
+    startBtn.textContent = "SPLIT";
+    if (!stopActive) {
+      stopActive = !stopActive;
+      stopBtn.textContent = "STOP";
+      stopBtn.className = "";
+    }
   } else {
-    clearInterval(id);
-    startBtn.textContent = "START";
-    flag = !flag;
+    createSplit();
   }
 };
 
 const stop = () => {
-  clearInterval(id);
-  seconds = 0;
-  startBtn.textContent = "START";
-  time.textContent = "---";
-  flag = true;
+  if (stopActive & !startActive) {
+    clearInterval(id);
+    stopActive = !stopActive;
+    stopBtn.textContent = "RESET";
+    stopBtn.className = "reset";
+    startActive = !startActive;
+    createSplit();
+    startBtn.textContent = "START";
+  } else {
+    startActive = true;
+    stopActive = true;
+    time.textContent = "---";
+    seconds = 0;
+    stopBtn.textContent = "STOP";
+    stopBtn.className = "";
+    startBtn.textContent = "START";
+    [...splits].forEach((item) => item.remove());
+  }
 };
-
 startBtn.addEventListener("click", start);
 stopBtn.addEventListener("click", stop);
